7448bf8c70338fe3849309ceb50dc017b33de803
AlexPan <ppgod@live.cn>
2018-03-19 15:09:34 +0800

    Change manual mining command name

--- cli/mining/mining.go
+++ cli/mining/mining.go
@@ -34,7 +34,7 @@ func miningAction(c *cli.Context) error {
 		return nil
 	}
 
-	if num := c.String("mine"); num != "" {
+	if num := c.String("number"); num != "" {
 		number, err := strconv.ParseInt(num, 10, 16)
 		if err != nil || number < 1 {
 			return errors.New("[number] must be a positive integer")
@@ -63,8 +63,8 @@ func NewCommand() *cli.Command {
 				Usage: "use --toggle [start, stop] to toggle cpu mining",
 			},
 			cli.StringFlag{
-				Name:  "mine, m",
-				Usage: "user --mine [number] to manual mine the given number of blocks",
+				Name:  "number, n",
+				Usage: "user --number [number] to manual mine the given number of blocks",
 			},
 		},
 		Action: miningAction,
