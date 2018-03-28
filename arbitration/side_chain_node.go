3a94416e09284d926e451f89361ff25d5b6514e5
Yan Minzhi <yanmingzhi@elastos.org>
2018-03-28 09:39:06 +0800

    Remove arbitration related interfaces.

--- arbitration/side_chain_node.go
+++
@@ -1,8 +0,0 @@
-package arbitration
-
-type SideChainNode interface {
-	GetCurrentHeight() (uint32, error)
-	GetBlockByHeight(height uint32) (BlockInfo)
-
-	SendTransaction(info *TransactionInfo) error
-}
