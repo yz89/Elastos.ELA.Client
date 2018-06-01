d99aac529ec8c567ce01ee2db92f3c2385e294bb
AlexPan <ppgod@live.cn>
2018-06-01 09:34:10 +0800

    show formated json data with all info commands

--- cli/info/info.go
+++ cli/info/info.go
@@ -7,6 +7,8 @@ import (
 	"github.com/elastos/Elastos.ELA.Client/rpc"
 
 	"github.com/urfave/cli"
+	"encoding/json"
+	"bytes"
 )
 
 func infoAction(c *cli.Context) error {
@@ -16,106 +18,112 @@ func infoAction(c *cli.Context) error {
 	}
 
 	if c.Bool("connections") {
-		result, err := rpc.CallAndUnmarshal("getconnectioncount", nil)
+		result, err := rpc.Call("getconnectioncount", nil)
 		if err != nil {
 			fmt.Println("error: get node connections failed,", err)
 			return err
 		}
-		fmt.Println(result)
+		printFormat(result)
 		return nil
 	}
 
 	if c.Bool("neighbor") {
-		result, err := rpc.CallAndUnmarshal("getneighbors", nil)
+		result, err := rpc.Call("getneighbors", nil)
 		if err != nil {
 			fmt.Println("error: get node neighbors info failed,", err)
 			return err
 		}
-		fmt.Println(result)
+		printFormat(result)
 		return nil
 	}
 
 	if c.Bool("state") {
-		result, err := rpc.CallAndUnmarshal("getnodestate", nil)
+		result, err := rpc.Call("getnodestate", nil)
 		if err != nil {
 			fmt.Println("error: get node state info failed,", err)
 			return err
 		}
-		fmt.Println(result)
+		printFormat(result)
 		return nil
 	}
 
 	if c.Bool("currentheight") {
-		result, err := rpc.CallAndUnmarshal("getcurrentheight", nil)
+		result, err := rpc.Call("getcurrentheight", nil)
 		if err != nil {
 			fmt.Println("error: get block count failed,", err)
 			return err
 		}
-		fmt.Println(result)
+		printFormat(result)
 		return nil
 	}
 
 	if c.Bool("getbestblockhash") {
-		result, err := rpc.CallAndUnmarshal("getbestblockhash", nil)
+		result, err := rpc.Call("getbestblockhash", nil)
 		if err != nil {
 			fmt.Println("error: get best block hash failed,", err)
 			return err
 		}
-		fmt.Println(result)
+		printFormat(result)
 		return nil
 	}
 
 	if index := c.Int64("getblockhash"); index >= 0 {
-		result, err := rpc.CallAndUnmarshal("getblockhash", rpc.Param("index", index))
+		result, err := rpc.Call("getblockhash", rpc.Param("height", index))
 		if err != nil {
 			fmt.Println("error: get block hash failed,", err)
 			return err
 		}
-		fmt.Println(result.(string))
+		printFormat(result)
 		return nil
 	}
 
 	if param := c.String("getblock"); param != "" {
 		index, err := strconv.ParseInt(param, 10, 64)
-		var result interface{}
 		if err == nil {
-			result, err = rpc.CallAndUnmarshal("getblockhash", rpc.Param("index", index))
+			result, err := rpc.CallAndUnmarshal("getblockhash", rpc.Param("height", index))
 			if err != nil {
 				fmt.Println("error: get block failed,", err)
 				return err
 			}
 			param = result.(string)
 		}
-		result, err = rpc.CallAndUnmarshal("getblock", rpc.Param("hash", param))
+		result, err := rpc.Call("getblock",
+			rpc.Param("blockhash", param).Add("verbosity", 2))
 		if err != nil {
 			fmt.Println("error: get block failed,", err)
 			return err
 		}
-		fmt.Println(result)
+		printFormat(result)
 		return nil
 	}
 
 	if param := c.String("gettransaction"); param != "" {
-		result, err := rpc.CallAndUnmarshal("getrawtransaction", rpc.Param("hash", param))
+		result, err := rpc.Call("getrawtransaction", rpc.Param("txid", param))
 		if err != nil {
 			fmt.Println("error: get transaction failed,", err)
 			return err
 		}
-		fmt.Println(result)
+		printFormat(result)
 		return nil
 	}
 
 	if c.Bool("showtxpool") {
-		result, err := rpc.CallAndUnmarshal("getrawmempool", nil)
+		result, err := rpc.Call("getrawmempool", nil)
 		if err != nil {
 			fmt.Println("error: get transaction pool failed,", err)
 			return err
 		}
-		fmt.Println(result)
+		printFormat(result)
 		return nil
 	}
 
 	return nil
+}
+
+func printFormat(data []byte) {
+	buf := new(bytes.Buffer)
+	json.Indent(buf, data, "", "    ")
+	fmt.Println(string(buf.Bytes()))
 }
 
 func NewCommand() *cli.Command {
